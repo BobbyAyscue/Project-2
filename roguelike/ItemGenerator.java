ff82356e3b8781052317085cf1db0aa098e8c07d
Abdullah <AbdullahAbdallatif21@gmail.com>
2021-11-19 04:47:25 +0000

    Added code, please tell me if I need to change anything

--- roguelike/ItemGenerator.java
+++ roguelike/ItemGenerator.java
@@ -2,11 +2,42 @@
 // this class contains a static method for creating items randomly
 
 import java.util.Random;
+import java,util.ArrayList;
 
 public class ItemGenerator {
-    public static Item generate() {
-        // TODO: replace this with your own code!
-        return new Item(ItemType.Other, "Vase", 10, 2, 0);
-    }
+	public static Item generate() {
+
+		// TODO: replace this with your own code!
+
+		Random rand = new Random();
+
+		ArrayList<String> weap = new ArrayList<String>();
+
+		weap.add("mace");
+		weap.add("Grenade");
+		weap.add("Flame Thrower");
+		weap.add("Rocket Launcher");
+		weap.add("Slingshot");
+		weap.add("minigun");
+
+		ArrayList<String> armor = new ArrayList<String>();
+		armor.add("Stealth Armor");
+		armor.add("Absorbtion Armor");
+		armor.add("Robotic Armor")
+		armor.add("Sheild");
+		armor.add("Padded armor");
+		armor.add("Health Regeneration Sheild");
+
+		ArrayList<String> other = new ArrayList<String>();
+
+		other.add("Pizza");
+		other.add("Flower");
+		other.add("wooden sword");
+		other.add("Lion");
+		other.add("Pepper Spray");
+		other.add("Knife");
+
+		return new Item(ItemType.Other, "Vase", 10, 2, 0);
+	}
 }
 
