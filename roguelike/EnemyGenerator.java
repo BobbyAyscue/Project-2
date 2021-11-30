e2735bdd1ba178e32bd13123e5811117584de331
Abdullah <AbdullahAbdallatif21@gmail.com>
2021-11-30 16:28:39 +0000

    Added 3 lines

--- roguelike/EnemyGenerator.java
+++ roguelike/EnemyGenerator.java
@@ -6,6 +6,10 @@ import java.util.Random;
 public class EnemyGenerator {
     public static Enemy generate(int row, int col) {
         // TODO: replace this with your own code!
+	// added 3 lines of code
+	return new Enemy("Snake", row, col, 2, 3,1);
+	return new Enemy("Cyclops", row, col, 10, 6,72);
+	return new Enemy("Gargoyle", row, col, 5, 2,4);
         return new Enemy("Goblin", row, col, 20, 5, 3);
     }
 }
