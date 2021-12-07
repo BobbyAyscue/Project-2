d1c5dde2210a854ece864f0b75f81a06fee2ef19
Abdullah <AbdullahAbdallatif21@gmail.com>
2021-12-07 05:40:58 +0000

    Added

--- roguelike/ItemGenerator.java
+++ roguelike/ItemGenerator.java
@@ -11,14 +11,9 @@ public class ItemGenerator
 
 
 
-		return new Item(ItemType.Other, "Mace", 15, 7, 1)
+
 		return new Item(ItemType.Other, "Vase", 10, 2, 0);
-		return new Item(ItemType.Other, "Torch", 6, 5, 3);
-		return new Item(ItemType.Other, "Bomb", 17, 10, 8);
-		return new Item(ItemType.Other, "Dagger", 8, 6, 3);
-		return new Item(ItemType.Other, "Mallet", 9, 4, 4);
-		return new Item(ItemType.Other, "Fist", 3, 1, 0);
-		return new Item(ItemType.Other, "Plank", 6, 3, 0);
+		
 
 
 	}
